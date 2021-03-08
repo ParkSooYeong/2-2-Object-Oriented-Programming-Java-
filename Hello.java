87870e5c4ba569256d04c46791ea4a31b6ee6c2f
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-03-08 13:48:31 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 2 , Semester 2 , Chapter 1 , Number 1 */
+
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Hello World");
+	}
+}
