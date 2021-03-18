359bed5b3c09ef22b7033d860bacd821aa9da65f
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-03-19 07:55:30 +0900

    Add files via upload

---
+++ 2-2-8-1 MyFrame.java
@@ -0,0 +1,20 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 2 , Semester 2 , Chapter 8 , Number 1 */
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class MyFrame extends JFrame
+{
+	public MyFrame()
+	{
+		setTitle("300x300 스윙 프레임 만들기");
+		setSize(300,300); // 프레임 크기 300x300
+		setVisible(true); // 프레임 출력
+	}
+	
+	public static void main(String[] args)
+	{
+		MyFrame frame = new MyFrame();
+	}
+}
