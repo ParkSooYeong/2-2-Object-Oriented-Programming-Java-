0863db41010c91065b29cd68047f1f1ee06e75f9
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-06-01 07:49:19 +0900

    Rename 2-2-4-006 Car.java to Chapter 4/2-2-4-006 Car.java

---
+++ Chapter 4/2-2-4-006 Car.java
@@ -0,0 +1,13 @@
+/* SKU CoE ITE : Object-Oriented Programming 실습(20200909) - 20170910 박수영 */
+
+package classEx; //패키지 이름 classEx
+
+public class Car //클래스 Car 선언
+{
+	String company; //문자열 회사 필드 company 선언
+	String model; //문자열 모델 필드 model 선언
+	String color; //문자열 색상 필드 color 선언
+	
+	int maxSpeed; //정수형 최고 속력 필드 maxSpeed 선언
+	int speed; //정수형 속도 필드 speed 선언
+}
