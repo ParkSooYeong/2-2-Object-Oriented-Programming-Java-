96b2f82166a27ed03007af62da1230bc81eae520
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-06-02 00:30:43 +0900

    Rename 2-2-5-3 Calculator.java to Chapter 5/2-2-5-3 Calculator.java

--- 2-2-5-3 Calculator.java
+++
@@ -1,9 +0,0 @@
-/* SKU CoE ITE - ParkSooYoung */
-/* Grade 2 , Semester 2 , Chapter 5 , Number 3 */
-
-abstract class Calculator
-{
-	public abstract int add(int a, int b); // 두 정수의 합을 구하여 리턴
-	public abstract int subtract(int a, int b); // 두 정수의 차를 구하여 리턴
-	public abstract double average(int[] a); // 정수 배열의 평균 리턴
-}
