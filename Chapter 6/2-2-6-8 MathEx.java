8e902d39d50ff1426babbc39e2c51c81d66a7617
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-06-03 10:24:21 +0900

    Rename 2-2-6-8 MathEx.java to Chapter 6/2-2-6-8 MathEx.java

---
+++ Chapter 6/2-2-6-8 MathEx.java
@@ -0,0 +1,20 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 2 , Semester 2 , Chapter 6 , Number 8 */
+
+public class MathEx
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Math.abs(-3.14)); // 절댓값 구하기
+		System.out.println(Math.sqrt(9.0)); // 제곱근
+		System.out.println(Math.exp(2)); // e의 2승
+		System.out.println(Math.round(3.14)); // 반올림
+		
+		// [1, 45] 사이의 정수형 난수 5개 발생
+		System.out.print("이번주 행운의 번호는 ");
+		for(int i=0; i<5; i++)
+		{
+			System.out.print((int)(Math.random()*45 + 1) + " "); // 난수 발생
+		}
+	}
+}
